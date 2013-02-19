41cce89d78d992101edd57f2bff4655bcbfceeb7
louis <louis@louis.(none)>
2013-02-19 17:23:31 -0500

    4.intermediate, 4.difficult

---
+++ louis/4/intermediate.py
@@ -0,0 +1,10 @@
+'''
+[2/12/2012] Challenge #4 [intermediate]
+create a calculator program that will take an input, following normal calculator input (5*5+4) and give an answer (29). This calculator should use all four operators.
+
+For extra credit, add other operators (6(4+3), 3 ** 3, etc.)
+'''
+
+# This challenge was implemented with javascript!  
+# The code is available under the jsCalculator repository
+# here: https://github.com/louiswilbrink/jsCalculator.git
